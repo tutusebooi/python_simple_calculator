6a8a4b598df72ce34dbf9783e4c03474b8613832
xolela <tutusebooi@gmail.com>
2024-08-24 22:37:05 +0200

    GUI window ADDED

--- calculator.py
+++ calculator.py
@@ -16,3 +16,22 @@ def user_input( num1,num2):
     return num1, num2
 
 
+def user_interfase():
+    root= tk.Tk()
+
+    root.geometry("500x500")
+
+    label = tk.Label(root, text="Calculator",font=("Bold", 15))
+    label.pack()
+    text_box = tk.Text(root, height=2,font=("bold",16))
+    text_box.pack(padx=10)
+
+    button = tk.Button(root,text="ENTER", font=("Bold", 18))
+    button.pack()
+    #tk.CENTER()
+
+
+    root.mainloop()
+user_interfase()
+
+
