425444a3745491fe11c1b53e8844f7a3b1fcc512
xolela <tutusebooi@gmail.com>
2024-08-24 22:23:09 +0200

    structure of operation functions

---
+++ opertations.py
@@ -0,0 +1,16 @@
+# operation function that we will import and use it in main file
+
+def sum_all(num1 , num2):
+    pass
+
+
+def minus_all(num1 , num2):
+    pass
+
+
+def multiply_all(num1 , num2):
+    pass
+
+
+def devide_all(num1 , num2):
+    pass
