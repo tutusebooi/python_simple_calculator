c4966b095ea620941e792da59de4532e653e9b3b
xolela <tutusebooi@gmail.com>
2024-08-24 22:28:22 +0200

    added some functionality

--- calculator.py
+++ calculator.py
@@ -1,11 +1,16 @@
 # simple calculator GUI
 import tkinter as tk
+from opertations import *
 
 def add_to_calculator(symbol):
     pass
 
 
 def clear_calculator():
+    pass
+
+
+def user_input():
     pass
 
 
